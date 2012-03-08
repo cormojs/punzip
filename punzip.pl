ab946c887dad246fef876c3d63d053de3d3fd298
cormo <cormorant2@gmail.com>
2012-03-08 20:08:11 +0900

    Fix: iconv conversion

--- punzip.pl
+++ punzip.pl
@@ -1,12 +1,10 @@
 #!/usr/bin/perl -sl
+use strict;
 use Archive::Zip;
 use Text::Iconv;
 
 # Pretend option
 our $p;
-
-# Create new archive
-our $c;
 
 # Encoding option
 our $encoding_from;
@@ -21,31 +19,17 @@ sub usage {
   print "  each default value of these is SJIS";
 }
 
-sub deflate {
-  my ($p, $files, $archive) = @_;
-  my $zip = Archive::Zip->new();
-
-  print "Compress to $archive...";
-  print "---";
-  foreach (@$files) {
-    print "Deflating... $filename";
-  }
-
-}
-
 sub inflate {
   my ($pretend, $archive, $encoder) = @_;
   my $zip = Archive::Zip->new();
 
-  $zip->read($archive) == AZ_OK
+  $zip->read($archive) == Archive::Zip->AZ_OK
     or die "Cannot read archive $archive.";
 
   print "Archive $archive:";
 
   foreach ($zip->members()) {
-    my $filename = (defined $encoder
-		    ? $encoder->convert($_->fileName())
-		    : $_->fileName());
+    my $filename = $encoder->convert($_->fileName());
 
     print "Inflating... $filename";
 
@@ -58,19 +42,7 @@ sub inflate {
 }
 
 sub main {
-  !defined $encoding_from
-    and $encoding_from = "SJIS";
-  !defined $encoding_to
-    and $encoding_to = "SJIS";
-
-  if (defined $c) {
-    deflate($p, \@ARGV, $c);
-  } else {
-    inflate($p, $ARGV[0],
-	    ($encoding_from eq $encoding_to
-	     ? Text::Iconv->new($encoding_from, $encoding_to)
-	     : undef));
-  }
+  inflate($p, $ARGV[0], Text::Iconv->new($encoding_from, $encoding_to));
 }
 
 main();
